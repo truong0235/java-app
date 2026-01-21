ea1f0e1fae2031a4993c3c47ed972729f992c171
truong0235 <53035575+truong0235@users.noreply.github.com>
2026-01-21 15:51:48 +0700

    change gradle file: use gradle run -Plaunch=(class import name) to run specific class example: gradle run -Plaunch=com.bat.Helper.DBConnectHeper fix path error todo: readd .vscode folder

--- app/src/main/java/com/bat/App.java
+++ app/src/main/java/com/bat/App.java
@@ -1,14 +1,6 @@
 package com.bat;
 
-import Utils.Helper.*;
-
 public class App {
     public static void main(String[] args) {
-        try {
-            DBConnectHelper conn = new DBConnectHelper();
-            System.out.println("It worked!");
-        } catch (Exception e) {
-            System.out.println("SQL error: " + e.getMessage());
-        }
     }
 }
---
+++ app/src/main/java/com/bat/Utils/Statics/Const.java
@@ -0,0 +1,10 @@
+package com.bat.Utils.Statics;
+
+public final class Const {
+    public static final String DBURL = "jdbc:mysql://cheemsserver:3306/";
+    public static final String DBNAME = "batdb";
+    public static final String DBUSERNAME = "root";
+    public static final String DBPASSWORD = "rootpassword";
+    public static final String DBDRIVER = "com.mysql.cj.jdbc.Driver";
+
+}
