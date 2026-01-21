b1a08c98ea794a936891f39d90cb44e264d6a511
truong0235 <53035575+truong0235@users.noreply.github.com>
2026-01-21 14:55:50 +0700

    ignore const

--- app/src/main/java/com/bat/Utils/Statics/Const.java
+++
@@ -1,9 +0,0 @@
-package Utils.Statics;
-
-public final class Const {
-    public static final String DBURL = "jdbc:mysql://localhost:3306/";
-    public static final String DBNAME = "batdb";
-    public static final String DBUSERNAME = "root";
-    public static final String DBPASSWORD = "rootpassword";
-    public static final String DBDRIVER = "com.mysql.cj.jdbc.Driver";
-}
