ee6ac4c751a06ecc06b66a90784b70bf3f432899
Hao <trannhuthao135@gmail.com>
2026-03-04 14:12:32 +0700

    Add files via upload

--- app/src/main/java/com/bat/DTO/ProductDTO.java
+++ app/src/main/java/com/bat/DTO/ProductDTO.java
@@ -16,6 +16,7 @@ public class ProductDTO {
     private int status;
 
     public ProductDTO() {}
+
     public ProductDTO(int productId, String productName, String pic, int categoryId, String publisher, int publishYear, String author, String language, BigDecimal price, int quantity, int status) {
         this.productId = productId;
         this.productName = productName;
@@ -30,111 +31,37 @@ public class ProductDTO {
         this.status = status;
     }
 
-    // Getters
-    public int getProductId() {
-        return productId;
-    }
+    // --- Getters & Setters ---
+    public int getProductId() { return productId; }
+    public void setProductId(int productId) { this.productId = productId; }
 
-    public String getProductName() {
-        return productName;
-    }
+    public String getProductName() { return productName; }
+    public void setProductName(String productName) { this.productName = productName; }
 
-    public String getPic() {
-        return pic;
-    }
+    public String getPic() { return pic; }
+    public void setPic(String pic) { this.pic = pic; }
 
-    public int getCategoryId() {
-        return categoryId;
-    }
+    public int getCategoryId() { return categoryId; }
+    public void setCategoryId(int categoryId) { this.categoryId = categoryId; }
 
-    public String getPublisher() {
-        return publisher;
-    }
+    public String getPublisher() { return publisher; }
+    public void setPublisher(String publisher) { this.publisher = publisher; }
 
-    public int getPublishYear() {
-        return publishYear;
-    }
+    public int getPublishYear() { return publishYear; }
+    public void setPublishYear(int publishYear) { this.publishYear = publishYear; }
 
-    public String getAuthor() {
-        return author;
-    }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
 
-    public String getLanguage() {
-        return language;
-    }
+    public String getLanguage() { return language; }
+    public void setLanguage(String language) { this.language = language; }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
+    public BigDecimal getPrice() { return price; }
+    public void setPrice(BigDecimal price) { this.price = price; }
 
-    public int getQuantity() {
-        return quantity;
-    }
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
 
-    public int getStatus() {
-        return status;
-    }
-
-    // Setters
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public void setPic(String pic) {
-        this.pic = pic;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public void setPublishYear(int publishYear) {
-        this.publishYear = publishYear;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    // toString
-    @Override
-    public String toString() {
-        return "ProductDTO{" +
-                "productId=" + productId +
-                ", productName='" + productName + '\'' +
-                ", pic='" + pic + '\'' +
-                ", categoryId=" + categoryId +
-                ", publisher='" + publisher + '\'' +
-                ", publishYear=" + publishYear +
-                ", author='" + author + '\'' +
-                ", language='" + language + '\'' +
-                ", price=" + price +
-                ", quantity=" + quantity +
-                ", status=" + status +
-                '}';
-    }
+    public int getStatus() { return status; }
+    public void setStatus(int status) { this.status = status; }
 }
